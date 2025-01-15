68d070b512e4405189d92a73f6b899e233857c78
madhukamethsara <methsaramadhuka@gmail.com>
2025-01-15 09:00:25 +0530

    change styling

--- js.js
+++ js.js
@@ -7,3 +7,9 @@ document.getElementById('about-button').addEventListener('click', function(e) {
         block: 'start' // Aligns the top of the section to the top of the viewport
     });
 });
+
+document.getElementById('scrollToProject').addEventListener('click', function (event) {
+    event.preventDefault(); // Prevent default anchor behavior
+    const projectSection = document.getElementById('project');
+    projectSection.scrollIntoView({ behavior: 'smooth' }); // Smooth scroll to the projects section
+});
